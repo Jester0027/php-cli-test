94372c155d40f7cfafbda884c3a621d2da95592f
Jester0027 <Paul.NED@outlook.com>
2020-08-11 19:25:05 +0200

    Custom namespace

--- src/Main.php
+++ src/Main.php
@@ -3,12 +3,17 @@
 namespace Cli;
 
 use Cli\Maker\ControllerMaker;
+use Cli\Traits\PathNormalizerTrait;
 
 class Main
 {
+    use PathNormalizerTrait;
+
     public static function main(int $argc, array $argv)
     {
-        $controllerMaker = new ControllerMaker('App\\Controller');
+        $config = (new Main())->getConfig();
+        $appNamespace = $config->applicationNamespace ?? "App";
+        $controllerMaker = new ControllerMaker("$appNamespace\\Controller");
         switch ($argv[1]) {
             case "make:controller":
                 $controllerMaker->createController($argv[2]);
@@ -17,5 +22,15 @@ class Main
                 echo 'World';
             default:
         }
+    }
+
+    public function getConfig(bool $assoc = false)
+    {
+        $projectDir = $this->wp_normalize_path(getcwd());
+        $file = $projectDir . '/cli-config.json';
+        if (file_exists($file)) {
+            return json_decode(file_get_contents($projectDir . '/cli-config.json'), $assoc);
+        }
+        return null;
     }
 }
