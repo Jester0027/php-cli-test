6015816ac28389c3dc1c6065b8e69f5e3d0474fb
Jester0027 <Paul.NED@outlook.com>
2020-08-11 19:24:21 +0200

    file check

--- src/Maker/ControllerMaker.php
+++ src/Maker/ControllerMaker.php
@@ -10,20 +10,21 @@ class ControllerMaker extends AbstractMaker
         $path = 'src/Controller/';
         $file = file_get_contents(__ROOT__ . '/templates/Controller');
         if (strpos($controllerName, '/')) {
-            var_dump($controllerName);
             $controllerNamespace = explode('/', $controllerName);
             $controllerName = end($controllerNamespace);
             array_pop($controllerNamespace);
             foreach ($controllerNamespace as $dir) {
                 $namespace .= "\\$dir";
             }
-            var_dump($namespace);
             $path = $path . implode('/', $controllerNamespace);
         }
 
         $file = str_replace('%NAMESPACE%', $namespace, $file);
         $file = str_replace('%CLASSNAME%', $controllerName, $file);
-        mkdir($this->projectPath . "/$path", 0755, true);
+        $absolutePath = $this->projectPath . "/$path";
+        if (!file_exists($absolutePath)) {
+            mkdir($absolutePath, 0755, true);
+        }
         $f = fopen($this->projectPath . "/{$path}/{$controllerName}Controller.php", 'w');
         fwrite($f, $file);
         fclose($f);
